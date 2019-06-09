ada180f2d20a99baab091a9f6ee7873850234be0
Emilijus Stankus <EMILIJUS@gmail.com>
2019-06-09 15:25:09 +0300

    Added Scorer entity

---
+++ VideoPoker/CombinationType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoPoker
+{
+    public enum CombinationType { None = 0, JacksOrBetter = 1, TwoPair = 2, ThreeOfAKind = 3,
+                                  Straight = 4, Flush = 6, FullHouse = 9, FourOfAKind = 25,
+                                  StraightFlush = 50, RoyalFlush = 800 };
+}
---
+++ VideoPoker/Scorer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoPoker
+{
+    class Scorer
+    {
+
+        public static CombinationType GetCombination(List<Card> hand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
