82a7858bad14380eb2ae27e166ddb0e4eb7a8b61
Emilijus Stankus <EMILIJUS@gmail.com>
2019-06-09 10:50:14 +0300

    Added Dealer entity

---
+++ VideoPoker/Dealer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoPoker
+{
+    class Dealer
+    {
+        private List<Card> deck;
+
+        public Dealer()
+        {
+            deck = new List<Card>();
+
+            var suits = Enum.GetValues(typeof(SuitType));
+            var values = Enum.GetValues(typeof(ValueType));
+
+            foreach (SuitType suit in suits)
+            {
+                foreach(ValueType value in values)
+                {
+                    deck.Add(new Card(suit, value));
+                }
+            }
+        }
+
+        public Card Draw()
+        {
+            if(deck.Count == 0)
+            {
+                throw new InvalidOperationException("Deck is empty.");
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
